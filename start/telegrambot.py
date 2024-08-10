7d10732a894f4cf50e9808927dffb0b727e74783
FelippeTN <felippenalim2004@gmail.com>
2024-08-10 15:50:52 -0300

    Base do Projeto

---
+++ start/telegrambot.py
@@ -0,0 +1,14 @@
+import os
+import telebot
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+bot = telebot.TeleBot(API_KEY)
+
+@bot.message_handler(commands=['Greet'])
+def greet(message):
+    bot.reply_to(message, 'Hey! Como você esta?')
+
+bot.polling()
