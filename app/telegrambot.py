94fcdcf62aa66f4533d9a4498da94cb4f6a815a3
FelippeTN <felippenalim2004@gmail.com>
2024-08-13 20:34:03 -0300

    logger configurado

--- app/telegrambot.py
+++ app/telegrambot.py
@@ -1,15 +1,33 @@
 import os
 import telebot
 from dotenv import load_dotenv
+import logging
+
 
 load_dotenv()
+
+# Configurar o logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+file_handler = logging.FileHandler("bot.log")
+file_handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+)
+logger = logging.getLogger(__name__)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
+
+# Suprimir logging excessivo da biblioteca httpx
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
 
 API_KEY = os.getenv('API_KEY')
 bot = telebot.TeleBot(API_KEY)
 
-@bot.message_handler(commands=['Greet'])
-def greet(message):
-    bot.reply_to(message, 'Hey! Como você esta?')
-
-print('Iniciando BOT')
-bot.polling()
+class TelegramBot:
+    def __init__(self):
+        logger.info("Inicializando o TelegramChatbot")
+        self.default_message_reply = (
+            "Olá! Gostaria de conversar com Nalim's BOT?"
+        )
